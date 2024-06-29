aff05b7c3bcf57f5ea59a073d53fb0eab1435b4f
simonivan32 <ivanarons@gmail.com>
2024-06-29 14:55:31 +0800

    Add files via upload

--- OnlyFunds.com/signup.php
+++ OnlyFunds.com/signup.php
@@ -1,9 +1,39 @@
+<?php
+    include("classes/connect.php");
+    include("classes/signupc.php");
+
+
+    
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        $signup = new Signup();
+        $result=  $signup->evaluate($_POST);
+        if($result!= ""){
+            
+            echo "<div style='text-align:center; font-size:12px;color:white;background-color:grey;'>";
+            echo "the following errors occurred:<br> <br>";
+            echo $result;
+            echo "</div>";
+
+        }
+        $username=$_POST['username'];
+        $email=$_POST['email'];
+        
+
+
+       // echo"<pre>";
+       // print_r($_POST);
+       // echo"</pre>";
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Signup</title>
 </head>
 
 <style>
@@ -60,11 +90,15 @@
     <div id="login_bar">
         <div style="font-size: 40px;">Sign Up<br></div>
         Sign up to join us<br><br>
-        <input type="text" id="text" placeholder="Username"><br><br>
-        <input type="text" id="text" placeholder="Email"><br><br>
-        <input type="text" id="text" placeholder="Password"><br><br>
-        <input type="text" id="text" placeholder="Retype Password"><br><br>
-        <input type="submit" id="button" value="Log in"><br><br>
+
+        <form method="post" action="">
+            <input value=<?php echo $username?>name="username" type="text" id="text" placeholder="Username"><br><br>
+            <input value=<?php echo $email?>name="email" type="text" id="text" placeholder="Email"><br><br>
+            <input name="password1" type="text" id="text" placeholder="Password"><br><br>
+            <input name="password2" type="text" id="text" placeholder="Retype Password"><br><br>
+            <input type="submit" id="button" value="Log in"><br><br>
+        </form>
+
 
         <div id="signup_button">
             Signup
